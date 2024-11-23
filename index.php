337372f8317fde2e2c5683972b024d691c52309c
Ryugi62 <xorjf1027@naver.com>
2024-11-23 17:08:01 +0900

    feat : 쇼핑몰 헤더 레이아웃 구현.

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- font import -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bokor&display=swap" rel="stylesheet">
+    
+    <!-- style import -->
+    <link rel="stylesheet" href="./style/index.css">
+
+    <!-- set title -->
+    <title>CREW_RIZE</title>
+</head>
+<body>
+    <header>
+        <div class="header continer">
+            <h1 class="logo"><a href="/">CREW RIZE</a></h1>
+    
+            <span class="menu-items">
+                <span class="menu-item">
+                    <input type="text" class="search_input" placeholder="RIZE의 상품을 검색하세요">
+                    <button class="search_button">search</button>
+                </span>
+    
+                <a href="./login.php">login</a>
+                |
+                <a href="./register.php">register</a>
+            </span>
+        </div>
+    </header>
+
+    <main>  
+    </main>
+
+    <footer></footer>
+</body>
+</html>
