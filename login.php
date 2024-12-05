87353b911a2a0b74fab322460688bb7f72e26bd7
Ryugi62 <xorjf1027@naver.com>
2024-12-05 17:36:20 +0900

    feat: 로그인 페이지에 폼 추가 및 스타일 개선

--- login.php
+++ login.php
@@ -14,11 +14,43 @@
             flex: 1;
 
             * {
-                border: 1px solid red;
+                /* border: 1px solid red; */
             }
 
             .login_view {
                 height: 100%;
+            }
+
+            form {
+                height: 100%;
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                padding-right: 150px;
+            }
+
+            .form-group {
+                gap: 1rem;
+                display: flex;
+                flex-direction: column;
+            }
+
+            input {
+                color: var(--black);
+                border: 1px solid var(--light-gray);
+                padding: 0.5rem;
+                border-radius: 0.5rem;
+                background-color: var(--light-gray);
+                width: 300px;
+            }
+
+            button {
+                color: #fff;
+                cursor: pointer;
+                border: 1px solid var(--light-gray);
+                padding: 0.5rem;
+                border-radius: 0.5rem;
+                background-color: var(--black);
             }
         }
     </style>
@@ -29,6 +61,24 @@
 
     <main>
         <div class="login_view">
+
+            <form action="/api/login.php" method="POST">
+                <div class="form-group">
+                    <label for="ID">아이디</label>
+                    <input type="text" name="ID" id="ID" required>
+
+                    <label for="password">비밀번호</label>
+                    <input type="password" name="password" id="password" required>
+
+                    <button type="submit">Login</button>
+
+                    <a href="register.php">Register</a>
+
+                    <a href="forgot-password.php">Forgot Password</a>
+                </div>
+            </form>
+
+
         </div>
     </main>
 
